2d748a070f399b80c22dd60a04510dd6a30eb435
fjorn-x <hamzashaikh00@gmail.com>
2022-06-24 23:32:02 +0530

    finished ui and working prototype

--- script.js
+++ script.js
@@ -1,43 +1,70 @@
-function computerPlay() {
+let playerCounter = 0;
+let computerCounter = 0;
+let tieCounter = 0;
+const playerResult = document.querySelector(".playerScore");
+const computerResult = document.querySelector(".computerScore");
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissor = document.querySelector(".scissor");
+const playerChoice = document.querySelector(".player-choice");
+const computerChoice = document.querySelector(".computer-choice");
+const computerSide = document.querySelector(".computer-side");
+const playerSide = document.querySelector(".player-side");
+
+function randomChoice() {
   let random = Math.floor(Math.random() * 3) + 1;
 
   if (random === 1) {
-    return "Rock";
+    return "ROCK";
   } else if (random === 2) {
-    return "Paper";
+    return "PAPER";
   } else {
-    return "Scissors";
+    return "SCISSOR";
   }
 }
 
-function playRound(playerSelection, computerSelection = computerPlay()) {
-  if (playerSelection === null) {
-    playerSelection = "Rock";
-  }
-  let playerOption =
-    playerSelection.charAt(0).toUpperCase() +
-    String(playerSelection).slice(1).toLowerCase();
-
-  if (playerOption === computerSelection) {
-    return `It's a tie since both players selected ${playerOption}`;
-  }
-  if (
-    (playerOption === "Rock" && computerSelection === "Scissors") ||
-    (playerOption === "Scissors" && computerSelection === "Paper") ||
-    (playerOption === "Paper" && computerSelection === "Rock")
+//game
+function playRound(playerSelection, computerSelection) {
+  playerChoice.textContent = playerSelection;
+  computerChoice.textContent = computerSelection;
+  if (playerSelection === computerSelection) {
+    //tie
+    tieCounter++;
+  } else if (
+    (playerSelection === "ROCK" && computerSelection === "SCISSOR") ||
+    (playerSelection === "SCISSOR" && computerSelection === "PAPER") ||
+    (playerSelection === "PAPER" && computerSelection === "ROCK")
   ) {
-    return `You Win ! ${playerOption} beats ${computerSelection}`;
+    //here player wins
+    playerCounter++;
   } else {
-    return `You Lose ! ${computerSelection} beats ${playerOption}`;
+    //here computer wins
+    computerCounter++;
+  }
+  playerResult.textContent = playerCounter;
+  computerResult.textContent = computerCounter;
+  if (playerCounter === 5) {
+    playerResult.textContent = "Wins";
+
+    playerCounter = 0;
+    computerCounter = 0;
+  } else if (computerCounter === 5) {
+    computerResult.textContent = "Wins";
+    computerCounter = 0;
+    playerCounter = 0;
   }
 }
 
-function game() {
-  for (i = 0; i < 5; i++) {
-    let playerOption = prompt("Select Rock, Paper or Scissors");
-    let result = playRound(playerOption);
-    console.log(result);
-  }
-}
-
-game();
+//recieves input from buttons
+rock.addEventListener("click", () => {
+  let computerSelection = randomChoice();
+  playRound(rock.value, computerSelection);
+});
+paper.addEventListener("click", () => {
+  let computerSelection = randomChoice();
+  playRound(paper.value, computerSelection);
+});
+scissor.addEventListener("click", () => {
+  let computerSelection = randomChoice();
+  playRound(scissor.value, computerSelection);
+});
